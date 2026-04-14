a9fc77ca88d81f96269f8b8a26f83ed42dbf0e10
Djalmarps <rdjalma97@gmail.com>
2026-04-14 19:39:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,13 @@ export default function App() {
         <h1>Portfólio de Djalma rodrigues</h1>
         <p>Desenvolvedor web em formação focado em React</p>
       </header>
-
+<section>
+  <h2>Sobre mim</h2>
+  <p>
+    Sou estudante de programação focado em desenvolvimento web.
+    Estou aprendendo React e criando projetos práticos.
+  </p>
+</section>
       <main>
         {/* Chamando o componente que contém a lista dinâmica */}
         <SecaoProjetos />
